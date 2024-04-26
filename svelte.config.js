993956139178d3e139e0edeb0087d3ea005df150
Deyvidas <106937559+Deyvidas@users.noreply.github.com>
2024-04-26 14:00:43 +0400

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,27 @@
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: preprocess({
+        scss: { prependData: `@import './static/index.scss';` }
+    }),
+
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(),
+
+        alias: {
+            'components/*': 'src/components/*'
+        }
+    },
+
+    onwarn: (warning, handler) => {
+        if (warning.code === 'css-unused-selector') return
+        handler(warning)
+    }
+}
